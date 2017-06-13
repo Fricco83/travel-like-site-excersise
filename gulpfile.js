9bb9debbae92c26aec7ed1ebc44d20f203ecd074
Peter <pktypa@gmail.com>
2017-06-13 22:48:24 +0200

    modify package.json and add styles

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var simplevars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var postcssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', function(){
+		console.log("it works");
+	});
+
+gulp.task('styles', function(){
+	return gulp.src('./app/assets/styles/style.css')
+	.pipe(postcss([postcssImport, simplevars, nested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('cssInject',['styles'], function(){
+		return gulp.src('./app/temp/styles/style.css')
+		.pipe(browserSync.stream());
+	});
+
+gulp.task('watch', function(){
+	browserSync.init({
+		server: {
+			baseDir: "app"
+		},
+		browser: "chrome",
+		notify: false
+	});
+
+	watch('./app/index.html', function(){
+		browserSync.reload();
+	});
+
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('cssInject');
+	});
+});
+
