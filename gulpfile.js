34290ad33b26b4913249f27d2775bd42901c64d0
Peter <pktypa@gmail.com>
2017-08-11 12:00:50 +0200

    finish header mobile

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,26 @@ var nested = require('postcss-nested');
 var postcssImport = require('postcss-import');
 var browserSync = require('browser-sync').create();
 var mixins = require('postcss-mixins');
+var webpack = require('webpack');
+
 
 gulp.task('default', function(){
 		console.log("it works");
 	});
+
+gulp.task('scripts', function(callback){
+	webpack(require('./webpack.config.js'), function(err, stats){
+		if(err) {
+			console.log(err.toString());
+		}
+		console.log(stats.toString());
+		callback();
+	});
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+	browserSync.reload();
+});
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/style.css')
@@ -38,6 +54,10 @@ gulp.task('watch', function(){
 
 	watch('./app/assets/styles/**/*.css', function(){
 		gulp.start('cssInject');
+	});
+
+	watch('./app/assets/scripts/**/*.js', function(){
+		gulp.start('scriptsRefresh');
 	});
 });
 
